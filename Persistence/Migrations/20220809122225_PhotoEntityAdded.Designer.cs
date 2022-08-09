ca1ae67915d06e7a7d87bc59c47172d0e850980f
Novinyo AMEGADJE <novinyolearn@gmail.com>
2022-08-09 13:55:17 +0000

    end of section 16

--- Persistence/Migrations/20220809122225_PhotoEntityAdded.Designer.cs
+++ Persistence/Migrations/20220809122225_PhotoEntityAdded.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220809122225_PhotoEntityAdded")]
+    partial class PhotoEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -145,8 +147,8 @@ namespace Persistence.Migrations
                     b.Property<string>("AppUserId")
                         .HasColumnType("varchar(255)");
 
-                    b.Property<bool>("IsMain")
-                        .HasColumnType("tinyint(1)");
+                    b.Property<string>("IsMain")
+                        .HasColumnType("longtext");
 
                     b.Property<string>("Url")
                         .HasColumnType("longtext");
---
+++ Persistence/Migrations/20220809123558_PhotoEntityIsMainNowBool.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class PhotoEntityIsMainNowBool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsMain",
+                table: "Photos",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsMain",
+                table: "Photos",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
